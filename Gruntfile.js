cf772631a22b653e60684e7b6ef3113f325e75ba
Mike <mikeh09@gmail.com>
2014-10-14 11:08:35 -0700

    added more upload files. cleaned up gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,139 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    concat: {
+      dist: {
+        src: ['public/client/**/*.js'],
+        dest: 'public/dist/built.js',
+      },
+    },
+
+    nodemon: {
+      dev: {
+        script: 'server.js'
+      }
+    },
+
+    uglify: {
+      options: {
+        mangle: false
+      },
+      my_target: {
+        files: {
+          'public/dist/output.min.js': ['public/dist/built.js']
+        }
+      }
+    },
+
+    jshint: {
+      files: {
+        src: ['app/**/*.js']
+        // Add filespec list here
+      },
+      options: {
+        force: 'true',
+        jshintrc: '.jshintrc',
+        ignores: [
+          'public/lib/**/*.js',
+          'public/dist/**/*.js'
+        ]
+      }
+    },
+
+    cssmin: {
+      my_target: {
+        files: [{
+          expand: true,
+          cwd: 'public',
+          src: ['*.css', '!*.min.css'],
+          dest: 'public/dist/',
+          ext: '.min.css'
+        }]
+      }
+    },
+
+    watch: {
+      scripts: {
+        files: [
+          'public/client/**/*.js',
+          'public/lib/**/*.js',
+        ],
+        tasks: [
+          'concat',
+          'uglify'
+        ]
+      },
+      css: {
+        files: 'public/*.css',
+        tasks: ['cssmin']
+      }
+    },
+    shell: {
+      scaleSite: {
+        command: "azure site scale mode standard shortlyMH"
+      },
+      gitAdd: {
+        command: "git add ."
+      },
+      gitCommit: {
+        command: "git commit"
+      },
+      gitPush: {
+        command: "git push azure2 master"
+      },
+      downScaleSite: {
+        command:"azure site scale mode free shortlyMH"
+      }
+    },
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-shell');
+  grunt.loadNpmTasks('grunt-nodemon');
+
+  grunt.registerTask('server', function (target) {
+    // Running nodejs in a different process and displaying output on the main console
+    var nodemon = grunt.util.spawn({
+         cmd: 'grunt',
+         grunt: true,
+         args: 'nodemon'
+    });
+    nodemon.stdout.pipe(process.stdout);
+    nodemon.stderr.pipe(process.stderr);
+
+    grunt.task.run([ 'watch' ]);
+  });
+  ////////////////////////////////////////////////////
+  // Main grunt tasks
+  ////////////////////////////////////////////////////
+
+  grunt.registerTask('test', [
+    'mochaTest'
+  ]);
+
+  grunt.registerTask('build', [
+    'concat',
+    'uglify',
+    'cssmin',
+    'jshint'
+  ]);
+
+  grunt.registerTask('upload', function(n) {
+    grunt.task.run(['shell:scaleSite','shell:gitAdd','shell:gitCommit','shell:gitPush','shell:downScaleSite']);
+  });
+
+  grunt.registerTask('deploy', function(){
+    grunt.task.run('build');
+    if(grunt.option('prod')) {
+      grunt.task.run('upload');
+    }
+    grunt.task.run('server');
+  });
+
+
+};
