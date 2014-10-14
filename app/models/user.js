d50c25d30db82625de7658fdad082efbe61beefa
Mike <mikeh09@gmail.com>
2014-10-14 10:45:02 -0700

    setup basic server files, haven't tested yet

---
+++ app/models/user.js
@@ -0,0 +1,17 @@
+var bcrypt = require('bcrypt-nodejs');
+var Promise = require('bluebird');
+var mongoose = require('mongoose');
+
+var userSchema = mongoose.Schema({
+    username: String,
+    password: String
+  })
+
+  userSchema.methods = {
+    initialize: function(){
+    this.on('creating', this.hashPassword);
+  }
+};
+
+var User = mongoose.model("User", userSchema);
+module.exports = User;
